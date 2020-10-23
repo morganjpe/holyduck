3c7e2463831150b0186b56240dabb17326b6fb2f
Evans <evansm54@GBSFYVL001M7922.local>
2020-10-23 15:49:38 +0100

    initial commit

---
+++ src/mocks/api-mock.js
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: "product name",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    price: 10.99,
+    stock: 10,
+    img: [
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636",
+    ],
+    allergens: ["nuts", "soy", "yeast"],
+  },
+  {
+    name: "Another Product Name",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    price: 15.0,
+    stock: 0,
+    img: [
+      "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636",
+    ],
+    allergens: ["nuts"],
+  },
+];
