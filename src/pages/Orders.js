79a158e02a9e0b72b06a370e2954d3b8fded8c3f
Evans <evansm54@GBSFYVL001M7922.local>
2020-11-08 14:37:03 +0000

    order management

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState, createContext} from 'react';
 import axios from 'axios';
-import {styled} from 'twin.macro';
+import tw,{styled, theme} from 'twin.macro';
+import propTypes from 'prop-types';
 // import socketIOClient from 'socket.io-client';
 
 const wait = time => new Promise((resolve) => setTimeout(resolve, time));
@@ -35,9 +36,7 @@ const AuthProvider = ({children}) => {
     )
 }
 
-
 const OrderList = ({orders, subKey}) => {
-
     return JSON.parse(orders)
         .map(({name, price, quantity, id}) => {
             return(
@@ -46,7 +45,11 @@ const OrderList = ({orders, subKey}) => {
                 </span>
             )
         })
+}
 
+OrderList.propTypes = {
+    orders: propTypes.string.isRequired,
+    subKey: propTypes.string.isRequired,
 }
 
 const AddressList = ({address}) => {
@@ -61,8 +64,51 @@ const AddressList = ({address}) => {
     );
 }
 
-const Orders = () => {
+AddressList.propTypes = {
+    address: propTypes.string.isRequired,
+}
 
+const StatusInput = ({status, action, checked}) => {
+    return(
+        <StatusInput.group status={status}>
+            <input 
+                checked={checked === status}
+                type="radio" 
+                name="status" 
+                id={status} 
+                value={status} 
+                onChange={action} />
+            <label htmlFor={status}>{status}</label>
+        </StatusInput.group>
+    )
+}
+
+StatusInput.group = styled.div`
+    padding: 30px;
+
+    input {
+        display: none;
+    }
+
+    label {
+        padding: 10px 20px;
+        cursor: pointer;
+    }
+
+    input:checked ~ label {
+        color: white;
+        border-radius: 7px;
+        background: ${({theme}) => theme.colors.hd_red};
+    }
+`;
+
+StatusInput.propTypes = {
+    status: propTypes.string.isRequired,
+    action: propTypes.func.isRequired,
+}
+
+const Orders = () => {
+    
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
@@ -77,23 +123,47 @@ const Orders = () => {
             .then(({data}) => setOrders(data))
             .catch(err => console.log(err))
     }
+
     // useEffect(() => {
     //     const socket = socketIOClient('http://localhost:3001');
     //     console.log(socket);
     //     socket.on('order_views', data => {
-
     //         const date = new Date();
-
     //         const order = {
     //             orderDetails: JSON.parse(data),
     //             orderTime: `${date.getHours()}:${date.getMinutes()}`
     //         }
-
     //         console.log(order);
-        
     //         setOrders([...orders, order]);
     //     })
     // });
+
+    const updateOrder = (e, ref) => {
+
+        const value = e.target.value;
+        console.log(ref);
+
+        axios.put(`http://localhost:3001/orders${ref}`, {
+            status:e.target.value
+        })
+        .then(({data}) => {
+            if(data.updated === ref) {
+                console.log(value);
+                getAllOrders();
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+    const deleteOrder = (ref) => {
+        axios.delete(`http://localhost:3001/orders${ref}`)
+            .then(({data}) => {
+                if(data.deleted === ref) {
+                    getAllOrders();
+                }
+            })
+        .catch(err => console.log(err));
+    }
 
     return(
         <>
@@ -102,12 +172,19 @@ const Orders = () => {
                     {orders.map(order => {
                         return(
                             <Orders.order key={order.reference}>
-                                <h4>{order.reference}</h4>
+                                <h4>Order reference - {order.reference}</h4>
                                 <OrderList 
                                     orders={order.order} 
                                     subKey={order.reference} />
                                 <br />
                                 <AddressList address={order.address} />
+                                <br />
+                                <Orders.status>
+                                    <StatusInput checked={order.status} status="processing" action={(e) => updateOrder(e, order.reference)} />
+                                    <StatusInput checked={order.status} status="confirmed" action={(e) => updateOrder(e, order.reference)} />
+                                    <StatusInput checked={order.status} status="delivering" action={(e) => updateOrder(e, order.reference)} />
+                                    <StatusInput checked={order.status} status="complete" action={() => deleteOrder(order.reference)} />
+                                </Orders.status>
                             </Orders.order>
                         )
                     })}
@@ -129,6 +206,10 @@ Orders.order = styled.li`
     padding:  0 15px 15px 15px;
     border: 1px solid #eee;
     margin-bottom: 15px;
+`;
+
+Orders.status = styled.div`
+    ${tw`flex`}
 `;
 
 
